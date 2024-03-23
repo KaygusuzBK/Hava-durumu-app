00dd7c75056161be4ec827e79869c93500d9ea9a
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2024-03-23 22:41:21 +0300

    Layout ve Router yapısı kuruldu

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import routes from "~/routes/index.jsx";
+
 import "./assets/Css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={routes} />
 );
---
+++ src/routes/index.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "~/layout/index.jsx";
+import Home from "~/pages/Home";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <div>About</div>,
+      },
+    ],
+  },
+]);
+
+export default routes;
