8060e583150a13d3c3b96e81b93c96adb968174b
Berkan Kaygusuz <berkankaygusuz41@outlook.com>
2024-03-26 20:37:18 +0300

    Renk Tanımlamaları

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,25 @@ export default {
   theme: {
     extend: {
       colors: {
+        myGray: {
+          900: "#13131A",
+          800: "#16161F",
+          700: "#1C1C27",
+          600: "#22222F",
+          500: "#3B3B54",
+          400: "#7F7F98",
+          300: "#ABABC4",
+          200: "#BFBFD4",
+          100: "#FAFAFA",
+          white: "#FFFFFF",
+        },
         searchBar: {
           bg: "#1E1E29",
           border: "#d1d5db",
           text: "#374151",
+        },
+        Product: {
+          blueLight: "#8FB2F5",
         },
       },
     },
